07d2786446510beaf684fc2ee57d66b9944f18e0
yuto.komai <yuto.komai@aktsk.jp>
2021-10-04 00:01:49 +0900

    animation section

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,126 @@
+// #docregion ShakeCurve
+import 'dart:math';
+
+// #enddocregion ShakeCurve
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const LogoApp());
 
-// TapboxA manages its own state.
+// #docregion diff
+class AnimatedLogo extends AnimatedWidget {
+  const AnimatedLogo({Key? key, required Animation<double> animation})
+      : super(key: key, listenable: animation);
 
-//------------------------- TapboxA ----------------------------------
-
-class TapboxA extends StatefulWidget {
-  const TapboxA({Key? key}) : super(key: key);
-
-  @override
-  _TapboxAState createState() => _TapboxAState();
-}
-
-class _TapboxAState extends State<TapboxA> {
-  bool _active = false;
-
-  void _handleTap() {
-    setState(() {
-      _active = !_active;
-    });
-  }
+  // Make the Tweens static because they don't change.
+  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
+  static final _sizeTween = Tween<double>(begin: 0, end: 300);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _handleTap,
-      child: Container(
-        child: Center(
-          child: Text(
-            _active ? 'Active' : 'Inactive',
-            style: const TextStyle(fontSize: 32.0, color: Colors.white),
-          ),
-        ),
-        width: 200.0,
-        height: 200.0,
-        decoration: BoxDecoration(
-          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+    final animation = listenable as Animation<double>;
+    return Center(
+      child: Opacity(
+        opacity: _opacityTween.evaluate(animation),
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          height: _sizeTween.evaluate(animation),
+          width: _sizeTween.evaluate(animation),
+          child: const FlutterLogo(),
         ),
       ),
     );
   }
 }
 
-//------------------------- MyApp ----------------------------------
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class LogoApp extends StatefulWidget {
+  const LogoApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Demo'),
-        ),
-        body: const Center(
-          child: TapboxA(),
-        ),
-      ),
-    );
+  _LogoAppState createState() => _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // #docregion AnimationController, tweens
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    // #enddocregion AnimationController, tweens
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
+    controller.forward();
   }
+
+  @override
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
+// #enddocregion diff
+
+// Extra code used only in the tutorial explanations. It is not used by the app.
+
+class UsedInTutorialTextOnly extends _LogoAppState {
+  UsedInTutorialTextOnly() {
+    // ignore: unused_local_variable, prefer_typing_uninitialized_variables
+    var animation, sizeAnimation, opacityAnimation, tween, colorTween;
+
+    // #docregion CurvedAnimation
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+    // #enddocregion CurvedAnimation
+
+    // #docregion tweens
+    sizeAnimation = Tween<double>(begin: 0, end: 300).animate(controller);
+    opacityAnimation = Tween<double>(begin: 0.1, end: 1).animate(controller);
+    // #enddocregion tweens
+
+    // #docregion tween
+    tween = Tween<double>(begin: -200, end: 0);
+    // #enddocregion tween
+
+    // #docregion colorTween
+    colorTween = ColorTween(begin: Colors.transparent, end: Colors.black54);
+    // #enddocregion colorTween
+  }
+
+  usedInTutorialOnly1() {
+    // #docregion IntTween
+    AnimationController controller = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this);
+    Animation<int> alpha = IntTween(begin: 0, end: 255).animate(controller);
+    // #enddocregion IntTween
+    return alpha;
+  }
+
+  usedInTutorialOnly2() {
+    // #docregion IntTween-curve
+    AnimationController controller = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this);
+    final Animation<double> curve =
+        CurvedAnimation(parent: controller, curve: Curves.easeOut);
+    Animation<int> alpha = IntTween(begin: 0, end: 255).animate(curve);
+    // #enddocregion IntTween-curve
+    return alpha;
+  }
+}
+
+// #docregion ShakeCurve
+class ShakeCurve extends Curve {
+  @override
+  double transform(double t) => sin(t * pi * 2);
 }
